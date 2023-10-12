541a5ad7519f43f9c06431a0d84ea6719d9c9f13
lucas rodrigues <lucashenriquerodrigues055@gmail.com>
2023-10-12 17:48:50 -0300

    primeiro commit

---
+++ src/data/words.js
@@ -0,0 +1,12 @@
+export const wordsList = {
+    carro: ["Motor", "Porta", "Capo", "Pneu", "Antena"],
+    fruta: ["Banana", "Maça", "Pera", "Mamao", "Laranja"],
+    corpo: ["Braço", "Perna", "Cerebro", "Pescoço", "Olhos"],
+    computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+    alimento: ["Arroz", "Feijao", "Carne", "Leite", "Ovo"],
+    escola: ["Lapis", "estojo", "lousa", "professor", "carteira"],
+    objeto: ["copo", "armario", "luminaria", "televisao", "estante"],
+    animal: ["hipopotamo", "cachorro", "papagaio", "peixe", "jabuti"],
+    pais: ["espanha", "alemanha", "brasil", "canada", "australia"],
+    cor: ["amarelo", "azul", "marrom", "vermelho", "preto"]
+  };
